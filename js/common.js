de78f71a86e00e69b47fb0160e4f1899481f84de
baicizhanTeam <2377694643@qq.com>
2015-07-19 21:35:09 +0800

    功能增加

--- js/common.js
+++ js/common.js
@@ -1,57 +1,76 @@
+/* Create by zhangmingxin at 2015/7/19*/
 
+/**
+ * 数据仓库 dataWarehouse ,用于存储通过ajax传递的数据以及本地的选择操作
+ */
+var dataWH={
+	NOTEBOOK: "",
+	NOTE: "",
+	CUR_NOTEBOOK: "",
+	CUR_NOTE: ""
+};
 
 var _this=null;
-var NOTE=diaryBook[0];
+
 init();
 /**
- *初始化
+ * 初始化 
+ * 指定一个 note对象作为全局对象，在所有可操作的文件中都起作用
  */
 function init() {
 	_this=this;
 	window.note=_this;
+	//将数据缓存起来
+	dataWH.NOTEBOOK=diaryBook;
+	dataWH.NOTE=diary;
+	dataWH.CUR_NOTEBOOK=dataWH.NOTEBOOK[0];
+	dataWH.CUR_NOTE=dataWH.NOTE[dataWH.NOTEBOOK[0]][0];
+
 	_this.initNoteCate();
 }
-/**
- *初始化笔记本
- */
+//初始化笔记本
 function initNoteCate() {
-	if(!diaryBook){
+	if(!dataWH.NOTEBOOK){
 		return;
 	}
 	var cates=$("#cates");
-	var len=diaryBook.length;
+	cates.empty();
+	var len=dataWH.NOTEBOOK.length;
 	for(var i=0;i<len;i++) {
-		if(i===0) {
-			_this.initNoteThumb(diaryBook[i]);
+		if(!dataWH.NOTE[dataWH.NOTEBOOK[i]]){ //判断相应笔记本是否为空
+			cates.append(_this.htmlModel().noteBookModel(dataWH.NOTEBOOK[i],0));
+		}else{
+			cates.append(_this.htmlModel().noteBookModel(dataWH.NOTEBOOK[i],dataWH.NOTE[dataWH.NOTEBOOK[i]].length));
+		} 	
+		if(dataWH.CUR_NOTEBOOK===dataWH.NOTEBOOK[i]) {
+			$("#cates>li:eq("+i+")").css("color","red");	
+			_this.initNoteThumb(dataWH.NOTEBOOK[i]);
 		}
-	 	cates.append(_this.htmlModel().noteBookModel(diaryBook[i],diary[diaryBook[i]].length));
 	}
 }
-/**
- *初始化笔记列表
- */
+//初始化笔记列表
 function initNoteThumb(data) {
-	if(!diary[data]) {
+	if(!dataWH.NOTE[data]) {
 		return;
 	}
-	NOTE=data;
+	dataWH.CUR_NOTEBOOK=data;
 	var notes=$("#notes");
 	notes.empty();
-	var len=diary[data].length;
+	var len=dataWH.NOTE[data].length;
+	if(len===0){
+		$("#noteDetail").empty();
+		_this.router("noteDetail",1);
+	}
 	for(var i=0;i<len;i++) {
-		var value=diary[data][i];
+		var value=dataWH.NOTE[data][i];
 		if(i===0){
 			_this.initNoteDetail(value);
-		}	
-		console.log(data);
+		}
 		notes.append(_this.htmlModel().noteModel(value.title,value.time,value.content,data));
 	}
 }
-/**
- *	初始化笔记内容
- */
+//初始化笔记内容
 function initNoteDetail(data){
-	console.log(data);
 	if(!data){
 		return;
 	}
@@ -59,16 +78,14 @@ function initNoteDetail(data){
 	noteDetail.empty();
 	noteDetail.append(_this.htmlModel().detaileModel(data.title,data.time,data.content));
 }
-/**
- *选择笔记本
- */
+//选择笔记本
 function selectNoteCate(e){
+	$(e).css("color","red").siblings("li").css("color","#CCC");
 	var data=$(e).children("span").text();
+	dataWH.CUR_NOTEBOOK=data;
 	_this.initNoteThumb(data);
 }
-/**
- *选择笔记
- */
+//选择笔记
 function selectNoteThumb(e){
 	$("#noteDetail").css("display","block");
 	$("#noteEdit").css("display","none");
@@ -77,24 +94,105 @@ function selectNoteThumb(e){
 	for(var i=0;i<notes.length;i++){
 		var data2=notes.eq(i).children("h6").text();
 		if(data2===data1){
-			_this.initNoteDetail(diary[NOTE][i]);
+			_this.initNoteDetail(dataWH.NOTE[dataWH.CUR_NOTEBOOK][i]);
 		}
 	}
 }
-/**
- * 新建笔记
- */
-function noteEdit(){
-	$("#noteDetail").css("display","none");
-	$("#noteEdit").css("display","block");
-	$("#editBtn").click(function(){
-		$("#edit").show();
-		$("#view").hide()
-	});
-	$("#viewBtn").click(function(){
-		$("#edit").hide();
-		$("#view").show();
-	});
+//添加笔记本
+function addNoteCate(type) {
+	switch(type) {
+		case 0:
+			$("#cates>div").remove(); //取消添加
+			break;
+		case 1:  //添加按钮
+		var cates=$("#cates");
+			cates.children("div").remove();
+			cates.append(_this.htmlModel().addnoteBookModel());
+			break;
+		case 2: //确认
+			var noteB=$("#noteB").val().trim();
+			if(noteB===""){
+				return;
+			}
+			dataWH.CUR_NOTEBOOK=noteB;
+			dataWH.NOTEBOOK.push(noteB);
+			dataWH.NOTE[noteB]=[];
+			_this.initNoteCate();
+			break;
+	}
+}
+//新建笔记
+function noteNew(){
+	_this.router("noteEdit",1);
+	_this.router("edit",0);
+	$("#editBtn").addClass("activeSpan").siblings("span").removeClass("activeSpan");
+}
+//编辑笔记	
+function noteEdit(e){
+	_this.router("edit",0);
+	$(e).addClass("activeSpan").siblings("span").removeClass("activeSpan");
+}
+//笔记解析
+function noteview(e){
+	_this.router("view",0);
+	$(e).addClass("activeSpan").siblings("span").removeClass("activeSpan");
+	//在这里添加解析内容
+	var tit=$("#noteTitle").val().trim();
+	var con=$("#noteContent").val().trim();
+	var view=$("#view");
+	view.empty();
+	view.append(_this.htmlModel().viewModel(tit,con));
+}
+//取消新建笔记
+function cancelNew(){
+		var tit=$("#noteTitle").val().trim();
+		var con=$("#noteContent").val().trim();
+		if(tit!=""||con!=""){
+			if(confirm("确认取消操作？？？")){
+				$("#noteTitle").val("");
+				$("#noteContent").val("");
+				$("#view").empty();
+				_this.router("noteDetail",1);
+			}
+		}else{
+				$("#view").empty();
+			_this.router("noteDetail",1);
+		}
+}
+//确认新建笔记
+function affirmNew(){
+	var tit=$("#noteTitle").val().trim();
+	var con=$("#noteContent").val().trim();
+	if(tit===""||con===""){
+		alert("请确认内容或者标题非空！");
+	}else{
+		dataWH.NOTE[dataWH.CUR_NOTEBOOK].push({"title":tit,"content":con,"time":"2015/9/10"});
+		_this.initNoteCate();
+		$("#noteTitle").val("");
+		$("#noteContent").val("");
+		$("#view").empty();
+		_this.router("noteDetail",1);
+	}
+}
+function router(data,cont){
+	var tag;
+	switch (cont){
+		case 0:
+			tag=["edit","view"];
+			break;
+		case 1:
+			tag=["noteDetail","noteEdit"];
+			break;
+		default:
+			break;
+	}
+	for(var i=0;i<tag.length;i++){
+		if(tag[i]===data){
+			$("#"+data).show();
+		}else{
+			$("#"+tag[i]).hide();
+		}
+	}
 }
 /**
  * html 模板
@@ -120,7 +218,34 @@ function htmlModel() {
 						 '<article>'+
 						 		'<div class="note-content"><p>'+content+'</p></div>'+
 						 	'</article>';
+		},
+		"addnoteBookModel": function(){
+			return '<div><input id="noteB" type="text">'+
+								'<span onclick="note.addNoteCate(2)">确认</span><span onclick="note.addNoteCate(0)">取消</span>'+
+							'</div>';
+		},
+		"viewModel": function(title,content){
+			return '<h3>'+title+'</h3><div><p>'+content+'</p></div>';
 		}
 	};
 	return hModel;
 };
+
+/**
+ *如果不是本地操作，就调用这个函数获取数据
+ */
+function ajax(action,param,callback,hideLoad){
+	$.ajax({
+		url: action,
+		data: param,
+		dataType: 'JSONP',
+		jsonpCallback: 'processJSON',
+		type: 'GET',
+		success: function (data) {
+      callback(data);
+    },
+    error: function(xhr, type){
+    	callback(type);
+    }
+	});
+}
--- js/data/data.js
+++ js/data/data.js
@@ -1,3 +1,8 @@
+/**
+ * json 格式的数据 ，用于本地操作
+ * 同时，若链接服务器，可直接使用ajax返回相同格式的数据即可用来操作和显示
+ * Create by zhangmingxin at 2015/7/19
+ */
 var diaryBook=["设计","体育锻炼","html5"];
 var diary= {
 	"设计": [
